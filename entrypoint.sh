a5c987d85f30d6ea78d764a2a3becf2854da26af
Josiah Choi <josiah.choi@diginex.com>
2018-10-19 18:13:53 +0800

    Removed echo.

--- entrypoint.sh
+++ entrypoint.sh
@@ -26,15 +26,12 @@ mkdir ~/.kube
 file="/conf/cluster.conf"
 if [ -f "$file" ]
 then
-	echo "$file found. Use cluster.conf."
     export KUBE_CLUSTER_NAME=$(prop 'kube_cluster_name')
     export KUBE_ENDPOINT_URL=$(prop 'kube_endpoint_url')
     export KUBE_BASE64_CA_CERT=$(prop 'kube_base64_ca_cert')
     export AWS_ACCESS_KEY_ID=$(prop 'aws_access_key_id') 
     export AWS_SECRET_ACCESS_KEY=$(prop 'aws_secret_access_key') 
     export AWS_DEFAULT_REGION=$(prop 'aws_default_region')
-else
-	echo "$file not found. Use Environment Variable."
 fi
 
 sed -e "s;%%%KUBE_ENDPOINT_URL%%%;`printenv KUBE_ENDPOINT_URL`;g" -e "s;%%%KUBE_BASE64_CA_CERT%%%;`printenv KUBE_BASE64_CA_CERT`;g" -e "s;%%%KUBE_CLUSTER_NAME%%%;`printenv KUBE_CLUSTER_NAME`;g" ./kube/kubeconfig.template > ~/.kube/config
