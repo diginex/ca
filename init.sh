07d024e95fb391f47db86e63b7a69eec19281e61
Josiah Choi <josiah.choi@diginex.com>
2018-10-23 14:13:50 +0800

    update cli description.

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-read -p "AWS Access Key:" aws_access_key_id </dev/tty
-read -p "AWS Secret Access Key:" aws_secret_access_key </dev/tty
-read -p "AWS Default Region (e.g. us-east-1):" aws_default_region </dev/tty
-read -p "EKS Cluster Name:" kube_cluster_name </dev/tty
-read -p "EKS Cluster Endpoint URL:" kube_endpoint_url </dev/tty
-read -p "EKS Cluster Base64 CA Cert.:" kube_base64_ca_cert </dev/tty
+read -p "AWS Access Key :" aws_access_key_id </dev/tty
+read -p "AWS Secret Access Key :" aws_secret_access_key </dev/tty
+read -p "AWS Default Region (e.g. us-east-1) :" aws_default_region </dev/tty
+read -p "EKS Cluster Name :" kube_cluster_name </dev/tty
+read -p "EKS Cluster Endpoint URL :" kube_endpoint_url </dev/tty
+read -p "EKS Cluster Base64 CA Cert. :" kube_base64_ca_cert </dev/tty
 
 mkdir -p ~/.ca
 echo "aws_access_key_id=$aws_access_key_id" > ~/.ca/cluster.conf
